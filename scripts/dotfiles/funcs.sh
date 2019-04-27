89eac52eeeac2f8a4df2f54e05113de06a8d04d5
Timm Bangma <bangma@hp.com>
2019-04-27 02:41:54 -0400

    added shortcut to start / stop / restart wander-common containers.

--- scripts/dotfiles/funcs.sh
+++ scripts/dotfiles/funcs.sh
@@ -10,3 +10,23 @@ wander-clone() {
   git clone "git@github.azc.ext.hp.com:Wander/$1.git" $folder_name
   cd $dir
 }
+
+start-wander-docker() {
+  dir=$(pwd)
+  cd ~/git/wander/common
+  rm ~/.wander_docker.log
+  dc up > ~/.wander_docker.log 2>&1 &
+  cd $dir
+}
+
+stop-wander-docker() {
+  dir=$(pwd)
+  cd ~/git/wander/common
+  dc down
+  cd $dir
+}
+
+restart-wander-docker() {
+  stop-wander-docker
+  start-wander-docker
+}
