4766b4354d021254aefbb16516526f850aced8ac
Timm Bangma <bangma@hp.com>
2019-04-29 20:23:35 -0400

    fixed kubectl version

--- scripts/dotfiles/versions.sh
+++ scripts/dotfiles/versions.sh
@@ -1,6 +1,6 @@
 export JDK_VER='11.0.2'
 export MVN_VER='3.6.1' 
 export COMPOSE_VER='1.23.1'
-export KUBECTL_VER='1.13.2'
+export KUBECTL_VER='1.13.2-00'
 export KOPS_VER='1.11.0'
 export HELM_VER='2.12.1'
