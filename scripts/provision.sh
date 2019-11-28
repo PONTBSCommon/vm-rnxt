341e08546cbbad84dbc6504943414d1f1e859efc
Jonathan Fritz <jonathan.fritz@hp.com>
2019-11-28 15:15:20 -0500

    CSOSCPS-7507: Fixed docker, got maven builds running on first login. Better documentation

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,6 +1,5 @@
-#############################################
-## #         Configure Settings          # ##
-#############################################
+# this script runs as root after the box boots for the first time
+# its goal is to apply any available updates to the base box, configure some permissions, and set up the .bashrc script for the vagrant user
 
 # functions used only here
 echo_success() { echo -e "\e[93m[$(date +"%H:%M:%S:%4N")] \e[39mdone. \e[32m(success)\e[39m"; }
@@ -11,13 +10,13 @@ export DEBIAN_FRONTEND=noninteractive
 export TERM=xterm
 
 # fix datetime.
-logf 'set proper timezone'
+logf '[01/19] set proper timezone'
 apt-get -qq install ifupdown >/dev/null && \
 timedatectl set-timezone America/Toronto && \
 echo_success || echo_failure 
 
 # install line ending convertor.
-logf 'installing dos2unix line ending convertor'
+logf '[02/19] installing dos2unix line ending convertor'
 apt-get -qq install dos2unix >/dev/null && \
 echo_success || echo_failure
 
@@ -25,13 +24,15 @@ echo_success || echo_failure
 tr -d '\15\32' < /home/vagrant/setup-win.sh > /home/vagrant/setup.sh && rm /home/vagrant/setup-win.sh
 
 # open ownership of the .aws folder
-logf 'set permissions on shared files and folders'
+logf '[03/19] set permissions on shared files and folders'
 chmod 777 /home/vagrant/.aws -R && chown vagrant:vagrant /home/vagrant/.aws -R && \
 chmod 777 /home/vagrant/setup.sh && chown vagrant:vagrant /home/vagrant/setup.sh && \
 echo_success || echo_failure
 
 # add custom profile my_bashrc.sh to bashrc
-logf 'add custom profile to ~/.bashrc'
+# this configures the vagrant user's shell whenever they log in and bootstraps the setup.sh script to run the first time 
+# the vagrant user logs into the box with `vagrant ssh`
+logf '[04/19] add custom profile to ~/.bashrc'
 echo '
 for file in $(ls ~/.dotfiles); do dos2unix ~/.dotfiles/$file >/dev/null; done
 for file in $(ls ~/.aws); do dos2unix ~/.aws/$file >/dev/null; done 
@@ -39,38 +40,45 @@ source /home/vagrant/.dotfiles/my_bashrc.sh
 ' >> /home/vagrant/.bashrc &&
 echo_success || echo_failure
 
-logf 'take ownership of the /usr/local/ folder.'
+logf '[05/19] take ownership of the /usr/local/ folder.'
 chown vagrant:vagrant /usr/local/ -R && \
 chmod +rwx /usr/local/ -R && \
 echo_success || echo_failure 
 
-logf 'let vagrant user sudo without password'
+# since this is just a development box, sudo just protects the vagrant user from themselves, so we may as well make it convenient to use
+logf '[06/19] let vagrant user sudo without password'
 echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers && \
 echo_success || echo_failure 
 
-logf 'adding authly to PATH'
+# the authly binary needs to be executable by any user from anywhere on the box
+logf '[07/19] add authly to $PATH'
 sudo cp /home/vagrant/bin/authly /usr/local/bin/authly && \
 sudo chmod 755 /usr/local/bin/authly && \
 echo_success || echo_failure
 
-#############################################
-## # System setup, and tool installation # ##
-#############################################
-printf 'updating package lists...' && \
-apt-get -q update >/dev/null #&& \
-printf 'updating system (this will take a few minutes)...' && \
+# later on, we'll install docker on the box. By default, it can only be used by the root user, but the vagrant user needs it too
+# see https://docs.docker.com/install/linux/linux-postinstall/ for details
+logf '[08/19] give vagrant permission to use docker'
+sudo groupadd docker && \
+sudo usermod -aG docker vagrant && \
+echo_success || echo_failure
+
+# apply any available updates to the box and remove any packages that are no longer required
+logf '[09/19] updating the system'
+apt-get -q update >/dev/null && \
 apt-get -qq upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" >/dev/null && \
-printf 'removing unused packages...' && \
 apt-get -qq autoremove -y >/dev/null && \
 echo_success || echo_failure 
 
-logf '[01] add printeron nameservers'
+# not sure if this is still required, but it should let the box get at the \\print share drive and PrinterOn Confluence
+logf '[10/19] add printeron nameservers'
 apt-get -qq install -y resolvconf ifupdown >/dev/null && \
 echo -e "nameserver 172.16.200.10\nnameserver 172.16.200.12" >> /etc/resolvconf/resolv.conf.d/head && \
 resolvconf -u && \
 echo_success || echo_failure 
 
-logf '[02] set permissions on copied in ssh keys.'
+# git is really picky about the permissions on ssh keys that it uses to authenticate with
+logf '[11/19] set permissions on copied in ssh keys'
 chmod 700 /home/vagrant/.ssh && \
 chmod 644 /home/vagrant/.ssh/known_hosts && \
 chmod 600 /home/vagrant/.ssh/id_rsa && \
@@ -78,15 +86,16 @@ chmod 644 /home/vagrant/.ssh/id_rsa.pub && \
 echo_success || echo_failure 
 
 MD5=$(ssh-keygen -l -E md5 -f /home/vagrant/.ssh/id_rsa.pub)
-printf 'the MD5 hash of your public key is %s' "$MD5"
+printf 'the MD5 hash of your public github key is %s' "$MD5"
 
-logf '[03] install oh-my-bash (pretty git prompt)'
+# the bash terminal is kind of ugly. this package makes it a little more bearable
+logf '[12/19] install oh-my-bash (pretty git prompt)'
 sudo -Hu vagrant sh -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)" <<< 'exit' && \
 cat /home/vagrant/.bashrc >> /home/vagrant/.bashrc.pre-oh-my-bash && \
 mv /home/vagrant/.bashrc.pre-oh-my-bash /home/vagrant/.bashrc -f && \
 echo_success || echo_failure
 
-logf '[04] clean up /tmp'
+logf '[13/19] clean up /tmp'
 rm -rf /tmp/* && \
 echo_success || echo_failure
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,7 @@
+# this script runs as vagrant the first time that user shells into the box with `vagrant ssh`
+# its goal is to run the wander-devbox build.sh script that installs all of the basic tooling, clone a few of the common 
+# repositories, and run the maven builds to ensure that everything is working as desired
+
 source ~/.bashrc
 
 # functions used only here
@@ -5,8 +9,8 @@ echo_success() { echo -e "\e[93m[$(date +"%H:%M:%S:%4N")] \e[39mdone. \e[32m(suc
 echo_failure() { echo -e "\e[93m[$(date +"%H:%M:%S:%4N")] \e[39merror \e[31m(failure)\e[39m"; }
 logf() { echo -e "\n\e[93m[$(date +"%H:%M:%S:%4N")]\e[95m === \e[1;39m$1\e[0;95m === \e[39m\e[21m\n"; }
 
-## This script clones the wander projects and downloads their dependencies on first run ##
-logf '[01] pull down the base wander projects.'
+## clones the wander projects and downloads their dependencies on first run
+logf '[14/19] pull down the base wander projects.'
 rm -rf ~/git/* && \
 cd ~/git && \
 git clone git@github.azc.ext.hp.com:Wander/wander-charts.git && \
@@ -16,19 +20,41 @@ git clone git@github.azc.ext.hp.com:Wander/wander-common.git && \
 git clone git@github.azc.ext.hp.com:Wander/wander-e2e-test.git && \
 echo_success || echo_failure
 
-logf '[02] run wander-devbox build.sh setup script'
+# runs the wander-devbox setup script, installing the base set of utilities
+logf '[15/19] run wander-devbox build.sh setup script'
 cd wander-devbox && \
 ./build.sh && \
 echo_success || echo_failure
 
-logf '[03] authenticate with ADFS'
+# fixes python path for docker and docker-compose - see https://github.com/docker/docker-py/issues/1502#issuecomment-418346541
+# this may need to be moved into wander-devbox/base.d/S21docker.sh if it affects linux/macos users that aren't in vagrant vms
+logf '[16/19] fix docker and docker-compose'
+sudo cp -r /usr/local/lib/python2.7/dist-packages/backports/ssl_match_hostname /usr/lib/python2.7/dist-packages/backports/ && \
+sudo cp -r /usr/local/lib/python2.7/dist-packages/backports/shutil_get_terminal_size /usr/lib/python2.7/dist-packages/backports/ && \
+echo_success || echo_failure
+
+# authenticates the user so that they can pull dependencies down from S3
+# requires user interaction
+logf '[17/19] authenticate with ADFS'
 authly && \
 echo_success || echo_failure
 
-logf '[04] pull common maven dependencies down from S3'
+# brings the docker containers from wander-common online. These are required to run tests for most wander microservices
+# the docker_login code was stolen from wander-common/run-docker.sh. It would be best to run that script directly, but it doesn't run the containers in daemon mode,
+# and for the purposes of this script, we need to background the containers so that we can finish the setup
+logf '[18/19] start wander-common docker containers'
+cd ~/git/wander-common && \
+docker_login=$(aws ecr get-login --region us-west-2 --no-include-email --registry-ids 037487371311) && \
+eval $docker_login && \
+docker-compose up -d && \
+echo_success || echo_failure
+
+# sets up maven and pulls a base set of dependencies down from S3 so that projects build faster
+# does not run E2E tests because they are slow and occasionally flake out!
+logf '[19/19] download maven dependencies down from S3'
 sudo cp ~/git/wander-devbox/base.d/settings.xml ~/.m2/settings.xml && \
-cd ~/git/wander-common && mvn clean install && \
-cd ~/git/wander-e2e-test && mvn clean install && \
+cd ~/git/wander-common && mvn clean test && \
+cd ~/git/wander-e2e-test && mvn clean compile && \
 echo_success || echo_failure
 
 cd ~ && logf '~(˘▾˘~) Installation is complete. Happy coding! (~˘▾˘)~'
